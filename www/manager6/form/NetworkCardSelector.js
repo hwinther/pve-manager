ac25e5d700f3fb5d1d6c8ba90d6244bc95cdcffa
Hans Christian Winther-Sørensen <hc@wsh.no>
2024-01-27 01:04:21 +0100

    Add support for sparc64

--- www/manager6/form/NetworkCardSelector.js
+++ www/manager6/form/NetworkCardSelector.js
@@ -9,5 +9,6 @@ Ext.define('PVE.form.NetworkCardSelector', {
 	['vmxnet3', 'VMware vmxnet3'],
 	["ne2k_pci", "RTL8029 NE2000 PCI"],
 	['pcnet', 'AMD PCnet FAST'],
+	['sunhme', 'Sun Happy Meal (sparc64 only)'],
     ],
 });
