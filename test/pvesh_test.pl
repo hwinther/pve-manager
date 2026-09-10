hwinther/pve-manager

commit 874d160f317a9100e5b37a00f18b86409ff0d5e8

pvesh: support for allOf/oneOf parameter schemas
pvesh wants to inject and pull out parameters from schemas quite a
bit. When oneOf/allOf schemas are involved this needs to be done a bit
more carefully.

Removal needs to recurse. This is not fully generalized as we know we
never want to remove from oneOf properties (as this is for URI-path
parameters).

Adding parameters could in theory be simple by always wrapping in an
allOf, but to limit the potential fall-out from turning *everything*
into an allOf schema type, we *merge* if we encounter an object,
append for allOfs, and wrap oneOfs in another allOf. This way, most
calls `pvesh` reaches should yield the same data as before.

Signed-off-by: Wolfgang Bumiller <w.bumiller@proxmox.com>
[TL: keep a usable path schema when there is no API path, carry over the
 target method's 'resolve_type' callback, and merge a oneOf into the copy]
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

PVE/CLI/pvesh.pm
@@ -2,6 +2,7 @@ package PVE::CLI::pvesh;
 
 use strict;
 use warnings;
+use Clone qw(clone);
 use HTTP::Status qw(:constants :is status_message);
 use String::ShellQuote;
 use PVE::JSONSchema qw(get_standard_option);
@@ -257,8 +258,6 @@ sub extract_path_info {
     return $info;
 }
 
-my $path_properties = {};
-
 my $api_path_property = {
     description => "API path.",
     type => 'string',
@@ -268,30 +267,100 @@ my $api_path_property = {
     },
 };
 
-my $uri_param = {};
-if (my $info = extract_path_info($uri_param)) {
-    foreach my $key (keys %{ $info->{parameters}->{properties} }) {
-        next if defined($uri_param->{$key});
-        $path_properties->{$key} = $info->{parameters}->{properties}->{$key};
+sub remove_schema_properties {
+    my ($schema, $to_remove) = @_;
+
+    if (my $props = $schema->{properties}) {
+        delete $props->{$_} for $to_remove->@*;
+    } elsif (my $nested = ($schema->{allOf} // $schema->{oneOf})) {
+        remove_schema_properties($_, $to_remove) for $nested->@*;
     }
 }
 
-$path_properties->{api_path} = $api_path_property;
-$path_properties->{noproxy} = {
-    description => "Disable automatic proxying.",
-    type => 'boolean',
-    optional => 1,
+my $uri_param = {};
+my $path_schema = { additionalProperties => 0, properties => {} };
+# We validate against a copy of the target method's schema, so we also need its method level
+# attributes. Without the 'resolve_type' callback a oneOf schema's type property would be
+# mandatory here, even though the endpoint itself lets clients omit it.
+my %path_method_info;
+if (my $info = extract_path_info($uri_param)) {
+    $path_schema = clone($info->{parameters});
+    remove_schema_properties($path_schema, [keys $uri_param->%*]);
+    if (my $resolve_type = $info->{resolve_type}) {
+        # the path parameters are not part of our schema, so add them back for the callback
+        $path_method_info{resolve_type} = sub {
+            my ($param) = @_;
+            return $resolve_type->({ $uri_param->%*, $param->%* });
+        };
+    }
+}
+
+my $our_properties = {
+    api_path => $api_path_property,
+    noproxy => {
+        description => "Disable automatic proxying.",
+        type => 'boolean',
+        optional => 1,
+    },
 };
+
+my sub merge_properties {
+    my ($from, $to) = @_;
+
+    if (my $props = $to->{properties}) {
+        $props->{$_} = $from->{$_} for keys $from->%*;
+        return;
+    }
+
+    $from = {
+        additionalProperties => 0,
+        properties => $from,
+    };
+
+    if (my $all_of = $to->{allOf}) {
+        push $all_of->@*, $from;
+    } elsif ($to->{oneOf}) {
+        my $old = { $to->%* };
+        $to->%* = (allOf => [$old, $from]);
+    } else {
+        die "unknown schema type (not an object?)\n";
+    }
+}
+
+merge_properties($our_properties, $path_schema);
 
 my $extract_std_options = 1;
 
+# Look for a property, but don't descend into oneOfs
+sub find_unconditional_schema_property {
+    my ($schema, $key) = @_;
+
+    if (my $props = $schema->{properties}) {
+        return $props->{$key};
+    } elsif (my $all_of = $schema->{allOf}) {
+        for my $subschema ($all_of->@*) {
+            my $p = find_unconditional_schema_property($subschema, $key);
+            return $p if defined($p);
+        }
+    }
+    return;
+}
+
 my $cond_add_standard_output_properties = sub {
-    my ($props) = @_;
+    my ($schema) = @_;
 
-    my $keys =
-        [grep { !defined($props->{$_}) } keys %$PVE::RESTHandler::standard_output_options];
+    my $keys = [
+        grep { !find_unconditional_schema_property($schema, $_) }
+            keys %$PVE::RESTHandler::standard_output_options
+    ];
 
-    return PVE::RESTHandler::add_standard_output_properties($props, $keys);
+    return $schema if !$keys->@*;
+
+    my $standard_props = PVE::RESTHandler::add_standard_output_properties(undef, $keys);
+
+    $schema = { $schema->%* };
+    merge_properties($standard_props, $schema);
+    return $schema;
 };
 
 my $handle_streamed_response = sub {
@@ -396,10 +465,8 @@ __PACKAGE__->register_method({
     path => 'ls',
     method => 'GET',
     description => "List child objects on <api_path>.",
-    parameters => {
-        additionalProperties => 0,
-        properties => $cond_add_standard_output_properties->($path_properties),
-    },
+    parameters => $cond_add_standard_output_properties->($path_schema),
+    %path_method_info,
     returns => { type => 'null' },
     code => sub {
         my ($param) = @_;
@@ -453,10 +520,8 @@ __PACKAGE__->register_method({
     path => 'get',
     method => 'GET',
     description => "Call API GET on <api_path>.",
-    parameters => {
-        additionalProperties => 0,
-        properties => $cond_add_standard_output_properties->($path_properties),
-    },
+    parameters => $cond_add_standard_output_properties->($path_schema),
+    %path_method_info,
     returns => { type => 'null' },
     code => sub {
         my ($param) = @_;
@@ -472,10 +537,8 @@ __PACKAGE__->register_method({
     path => 'set',
     method => 'PUT',
     description => "Call API PUT on <api_path>.",
-    parameters => {
-        additionalProperties => 0,
-        properties => $cond_add_standard_output_properties->($path_properties),
-    },
+    parameters => $cond_add_standard_output_properties->($path_schema),
+    %path_method_info,
     returns => { type => 'null' },
     code => sub {
         my ($param) = @_;
@@ -491,10 +554,8 @@ __PACKAGE__->register_method({
     path => 'create',
     method => 'POST',
     description => "Call API POST on <api_path>.",
-    parameters => {
-        additionalProperties => 0,
-        properties => $cond_add_standard_output_properties->($path_properties),
-    },
+    parameters => $cond_add_standard_output_properties->($path_schema),
+    %path_method_info,
     returns => { type => 'null' },
     code => sub {
         my ($param) = @_;
@@ -510,10 +571,8 @@ __PACKAGE__->register_method({
     path => 'delete',
     method => 'DELETE',
     description => "Call API DELETE on <api_path>.",
-    parameters => {
-        additionalProperties => 0,
-        properties => $cond_add_standard_output_properties->($path_properties),
-    },
+    parameters => $cond_add_standard_output_properties->($path_schema),
+    %path_method_info,
     returns => { type => 'null' },
     code => sub {
         my ($param) = @_;

commit 4ca480cdedc8ecc62e96e30303a35cfe4612c370

pvesh: allow endpoints with empty parameter schemas
The allOf/oneOf support assumes every plain parameter schema has a
properties member. Valid empty schemas, such as the firewall options
GET endpoint, omit it, so pvesh aborts during module loading before
calling the API.

Accept these schemas when adding CLI options, restoring parameterless
API calls without changing composite schema handling.

Fixes: 874d160f317a ("pvesh: support for allOf/oneOf parameter schemas")
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

PVE/CLI/pvesh.pm
@@ -307,7 +307,9 @@ my $our_properties = {
 my sub merge_properties {
     my ($from, $to) = @_;
 
-    if (my $props = $to->{properties}) {
+    if ($to->{properties} || !($to->{allOf} || $to->{oneOf})) {
+        # Plain parameter schemas may omit an empty property map.
+        my $props = $to->{properties} //= {};
         $props->{$_} = $from->{$_} for keys $from->%*;
         return;
     }
@@ -322,8 +324,6 @@ my sub merge_properties {
     } elsif ($to->{oneOf}) {
         my $old = { $to->%* };
         $to->%* = (allOf => [$old, $from]);
-    } else {
-        die "unknown schema type (not an object?)\n";
     }
 }
 

test/Makefile
@@ -5,7 +5,7 @@ all:
 export PERLLIB=..
 
 .PHONY: check
-check: test-replication test-balloon test-vzdump test-osd test-ceph-health test-ceph-auth test-ceph-key-migration test-ceph-lockbox-migration test-custom-cpu-models
+check: test-replication test-balloon test-vzdump test-osd test-ceph-health test-ceph-auth test-ceph-key-migration test-ceph-lockbox-migration test-custom-cpu-models test-pvesh
 
 .PHONY: test-balloon
 test-balloon:
@@ -52,6 +52,10 @@ test-ceph-lockbox-migration:
 .PHONY: test-custom-cpu-models
 test-custom-cpu-models:
 	./CustomCPUModels_test.pl
+
+.PHONY: test-pvesh
+test-pvesh:
+	./pvesh_test.pl
 
 .PHONY: install
 install:

test/pvesh_test.pl
@@ -1,0 +1,128 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use lib ('.', '..');
+
+use JSON;
+use Test::More;
+
+use PVE::Tools;
+
+# pvesh builds its command schemas at module load time from @ARGV, so each invocation needs
+# a fresh process. Keep the real firewall handler and mock only its configuration read.
+my $cli = <<'EOF';
+use strict;
+use warnings;
+
+use JSON;
+use Test::MockModule;
+
+use PVE::API2;
+use PVE::Firewall;
+use PVE::RPCEnvironment;
+
+my $schema = decode_json(shift @ARGV);
+if (defined($schema)) {
+    PVE::API2->register_method({
+        name => 'pvesh_test',
+        path => 'pvesh-test',
+        method => 'GET',
+        parameters => $schema,
+        returns => { type => 'object' },
+        code => sub { return shift; },
+    });
+}
+
+my $firewall_mock = Test::MockModule->new('PVE::Firewall');
+$firewall_mock->redefine(load_clusterfw_conf => sub { return { options => {} }; });
+PVE::RPCEnvironment->init('cli');
+
+require PVE::CLI::pvesh;
+
+my $cmd = shift @ARGV;
+PVE::CLI::pvesh->cli_handler("pvesh $cmd", $cmd, \@ARGV, ['api_path'], {});
+EOF
+
+my $object = {
+    additionalProperties => 0,
+    properties => {
+        value => { type => 'string' },
+    },
+};
+my $one_of = {
+    'type-property' => 'type',
+    'type-property-schema' => { type => 'string', enum => ['one', 'two'] },
+    oneOf => [
+        {
+            'instance-type' => 'one',
+            additionalProperties => 0,
+            properties => { first => { type => 'string' } },
+        },
+        {
+            'instance-type' => 'two',
+            additionalProperties => 0,
+            properties => { second => { type => 'string' } },
+        },
+    ],
+};
+
+my $tests = [
+    {
+        name => 'parameterless firewall options GET',
+        path => '/cluster/firewall/options',
+        expected => {},
+    },
+    {
+        name => 'empty schema without properties',
+        schema => { additionalProperties => 0 },
+        expected => {},
+    },
+    {
+        name => 'allOf properties',
+        schema => { allOf => [$object] },
+        args => ['--value', 'test'],
+        expected => { value => 'test' },
+    },
+    {
+        name => 'oneOf properties',
+        schema => $one_of,
+        args => ['--type', 'one', '--first', 'test'],
+        expected => { type => 'one', first => 'test' },
+    },
+];
+
+for my $test ($tests->@*) {
+    subtest $test->{name} => sub {
+        my ($output, $error) = ('', '');
+        my $cmd = [
+            $^X,
+            '-I.',
+            '-I..',
+            '-',
+            encode_json($test->{schema}),
+            'get',
+            $test->{path} // '/pvesh-test',
+            ($test->{args} // [])->@*,
+            '--output-format',
+            'json',
+        ];
+        eval {
+            PVE::Tools::run_command(
+                $cmd,
+                input => $cli,
+                outfunc => sub { $output .= "$_[0]\n"; },
+                errfunc => sub { $error .= "$_[0]\n"; },
+            );
+        };
+        is($@, '', 'CLI exits successfully') or diag($error);
+        is($error, '', 'no diagnostics');
+        my $result = eval { decode_json($output) };
+        is($@, '', 'output is JSON');
+        delete $result->{digest} if $test->{path} && ref($result) eq 'HASH';
+        is_deeply($result, $test->{expected}, 'API result excludes CLI-only parameters');
+    };
+}
+
+done_testing();